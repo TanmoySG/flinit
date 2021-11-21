b61e2285362fecaf03adc692b032c3aff1d48f46
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2021-11-21 16:46:56 +0000

    Published Version 0.1.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='flinit',
-    version='0.1.8',
+    version='0.1.9',
     author='Tanmoy Sen Gupta',
     author_email='tanmoysps@gmail.com',
     url='https://github.com/TanmoySG/flinit',
